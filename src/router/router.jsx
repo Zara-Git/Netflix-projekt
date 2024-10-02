16cccec355fe34b571a5798c5d53f8cbc541ac0d
Zara <idapple436@gmail.com>
2024-10-02 15:49:42 +0200

    Category navigation to router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,13 @@
 //Här skapar vi en router-konfiguration
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from '../views/HomePage';
+import HomePage from "../views/HomePage";
 import Movie from "../components/Movie/Movie";
+import Categories from "../pages/Categories/Categories";
+
 const router = createBrowserRouter([
-    { path: "/", element: <HomePage /> },
-    { path: "/movie/:title", element: <Movie/> },
-  ]);
+  { path: "/", element: <HomePage /> },
+  { path: "/category", element: <Categories /> },
+  { path: "/movie/:title", element: <Movie /> },
+]);
 
-
-
-
-  export default router;
+export default router;
