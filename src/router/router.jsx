59c9cb64b8f7aa06a90b3394a71cf0aa7dc00f3c
Diana-Lindberg <144327108+Diana-Lindberg@users.noreply.github.com>
2024-10-03 18:26:54 +0200

    update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,20 +1,18 @@
 //Här skapar vi en router-konfiguration
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from '../pages/HomePage';
-import FilmView from "../pages/FilmView/FilmView";
-import Categories from '../pages/Categories/Categories'
-import Movie from "../components/Movie/Movie";
 
+
+import HomePage from '../pages/HomePage';
+
+import Movie from "../components/Movie/Movie";
+import Categories from "../pages/Categories/Categories";
+import MovieDetails from "../pages/MovieDetailes/MovieDetailes";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/category", element: <Categories /> },
   { path: "/movie-details", element: <MovieDetails /> },
-  { path: "/film/:title", element: <Movie /> },
-  { path: "/movie/:title", element: <FilmView /> },
-  ]);
+  { path: "/movie/:title", element: <Movie /> },
+]);
 
-
-
-
-  export default router;
+export default router;
