044b3f4f4ce5d3c229e6c39d82f3d7d0a5493244
Diana-Lindberg <144327108+Diana-Lindberg@users.noreply.github.com>
2024-10-03 18:12:44 +0200

    Added path in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-import Categories from "./pages/Categories/Categories";
+
 
 function App() {
   return (
     <>
       <div>
         <RouterProvider router={router} />
-         <Categories /> 
       </div>
     </>
   );
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 import FilmView from "../pages/FilmView/FilmView";
+import Categories from '../pages/Categories/Categories'
 
 
 const router = createBrowserRouter([
@@ -9,11 +10,13 @@ const router = createBrowserRouter([
       path: "/", 
       element: <HomePage /> 
     },
-    // { path: "/movie/:title", element: <Movie/> },
-
     {
       path: '/movie/:title',
       element: <FilmView />
+    },
+    {
+      path: '/category',
+      element: <Categories />
     },
   ]);
 
