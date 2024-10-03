50d9ba1b78f84799730e125565dbc43a619e5805
Diana-Lindberg <144327108+Diana-Lindberg@users.noreply.github.com>
2024-10-03 18:24:19 +0200

    Update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -7,19 +7,11 @@ import Movie from "../components/Movie/Movie";
 
 
 const router = createBrowserRouter([
-    { 
-      path: "/", 
-      element: <HomePage /> 
-    },
-    {
-      path: '/movie/:title',
-      element: <FilmView />
-    },
-    {
-      path: '/category',
-      element: <Categories />
-    },
-    { path: "/film/:title", element: <Movie /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/category", element: <Categories /> },
+  { path: "/movie-details", element: <MovieDetails /> },
+  { path: "/film/:title", element: <Movie /> },
+  { path: "/movie/:title", element: <FilmView /> },
   ]);
 
 
