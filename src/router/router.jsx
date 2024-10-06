1a0e7de8a71b9614932861eeeb2d0ee713e1de35
Zara <idapple436@gmail.com>
2024-10-06 18:45:23 +0200

    updated of category style and test

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,16 +1,15 @@
 //Här skapar vi en router-konfiguration
 import { createBrowserRouter } from "react-router-dom";
 
-
-import HomePage from '../pages/HomePage';
+import HomePage from "../pages/HomePage";
 import Categories from "../pages/Categories/Categories";
-import MovieDetails from "../pages/MovieDetailes/MovieDetailes";
-import FilmView from '../pages/FilmView/FilmView'
+// import MovieDetails from "../pages/MovieDetailes/MovieDetailes";
+import FilmView from "../pages/FilmView/FilmView";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/category", element: <Categories /> },
-  { path: "/movie-details", element: <MovieDetails /> },
+  // { path: "/movie-details", element: <MovieDetails /> },
   { path: "/movie/:title", element: <FilmView /> },
 ]);
 
